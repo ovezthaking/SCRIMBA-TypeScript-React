d49db676e62c7784cf1b7311b3e149d8b7b2cc6a
ovezthaking <oliwerx12@gmail.com>
2025-11-19 22:09:13 +0100

    FINAL COMMIT: refactor: simplify boolean expression for last guess correctness

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ export default function AssemblyEndgame() {
     const isGameLost: boolean = wrongGuessCount >= numGuessesLeft
     const isGameOver: boolean = isGameWon || isGameLost
     const lastGuessedLetter: string = guessedLetters[guessedLetters.length - 1]
-    const isLastGuessIncorrect: boolean = lastGuessedLetter && !currentWord.includes(lastGuessedLetter)
+    const isLastGuessIncorrect: boolean = !!lastGuessedLetter && !currentWord.includes(lastGuessedLetter)
     
     // Static values
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
