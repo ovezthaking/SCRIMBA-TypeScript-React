157dc802dde99acb3089300db9f57a4d36faef1e
ovezthaking <oliwerx12@gmail.com>
2025-11-19 16:50:09 +0100

    feat: add components for game status, keyboard, language chips, and new game button

---
+++ src/components/ConfettiContainer.tsx
@@ -0,0 +1,17 @@
+import Confetti from "react-confetti"
+import type { JSX } from 'react'
+
+export default function ConfettiContainer({ isGameWon }): JSX.Element | null{
+    if (!isGameWon) {
+        return null
+    }
+    else {
+        return (
+            <Confetti
+                recycle={false}
+                numberOfPieces={1000}
+            />
+        )
+    }
+
+}   
